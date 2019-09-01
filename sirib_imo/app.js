050ca99c404de3deddd1b328804fa99ccca15fd8
shininghyunho <gusgh3315@uos.ac.kr>
2019-09-01 15:50:50 +0900

    sirib_imo_login

---
+++ sirib_imo/app.js
@@ -0,0 +1,106 @@
+/*jslint devel: true */
+/* eslint-disable no-console */
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+/**
+ * 20190828 단순한 로그인 기능만 구현함
+ * 
+ */
+
+//===== 모듈 불러들이기 =====//
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+
+var config = require('./config');
+var database = require('./database/database');
+var route_loader = require('./routes/route_loader');
+
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var expressErrorHandler = require('express-error-handler');
+
+
+
+//===== Express 서버 객체 만들기 =====//
+var app = express();
+
+
+//===== 뷰 엔진 설정 =====//
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+
+
+//===== 서버 변수 설정 및 static으로 public 폴더 설정  =====//
+console.log('config.server_port : %d', config.server_port);
+app.set('port', config.server_port);
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//===== body-parser, cookie-parser, express-session 사용 설정 =====//
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(expressSession({
+	secret:'my key',
+	resave:true,
+	saveUninitialized:true
+}));
+
+
+//===== 라우터 미들웨어 사용 =====//
+//app.use(app.router);
+
+//라우팅 정보를 읽어들여 라우팅 설정
+route_loader.init(app);
+
+
+
+
+//===== 404 에러 페이지 처리 =====//
+var errorHandler = expressErrorHandler({
+ static: {
+   '404': './public/404.html'
+ }
+});
+
+app.use( expressErrorHandler.httpError(404) );
+app.use( errorHandler );
+
+
+//===== 서버 시작 =====//
+
+//확인되지 않은 예외 처리 - 서버 프로세스 종료하지 않고 유지함
+process.on('uncaughtException', function (err) {
+	console.log('uncaughtException 발생함 : ' + err);
+	console.log('서버 프로세스 종료하지 않고 유지함.');
+	
+	console.log(err.stack);
+});
+
+// 프로세스 종료 시에 데이터베이스 연결 해제
+process.on('SIGTERM', function () {
+    console.log("프로세스가 종료됩니다.");
+    app.close();
+});
+
+app.on('close', function () {
+	console.log("Express 서버 객체가 종료됩니다.");
+	if (database.db) {
+		database.db.close();
+	}
+});
+
+// 시작된 서버 객체를 리턴받도록 합니다. 
+var server = http.createServer(app).listen(app.get('port'), function(){
+	console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
+
+	// 데이터베이스 초기화
+	database.init(app, config);
+   
+});
